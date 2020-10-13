985acb725263ecb3c7d172a8ee478fc7e507e9b0
Will09122000 <will0912@icloud.com>
2020-10-13 14:22:08 +0100

    Q2

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,32 +1,37 @@
 type Point
     = ( Int, Int )
 
-pretty :: [[a]] -> String
-pretty (x:xs)
+pretty :: [String] -> String
+pretty x
     -- = putStr(unlines x ++ unlines y ++ unlines z) 
-    = x >> pretty xs
+    = unlines x
 
 glider :: [ Point ]
 glider
     = [ (0, 2), (1, 3), (2, 1), (2, 2), (2, 3) ]
 
-isCell :: Int -> Int -> Point -> Bool
-isCell x y p
-    = if x == fst p && y == snd p
+
+isCell :: Int -> Int -> [Point] -> Bool
+isCell x y []
+    = False
+isCell x y (p:ps) = 
+    if x == fst p && y == snd p
         then True
+    else
+        isCell x y ps
 
 createRow :: Int -> Int -> [Point] -> String
-createRow 0 y p:ps
+createRow (-1) y p
     = []
-createRow x y p:ps
-    | isCell x y p == True = "#" ++ createRow (x-1) y ps
-    | otherwise = "." ++ createRow (x-1) y ps
+createRow x y p
+    | isCell x y p = "#" ++ createRow (x-1) y p
+    | otherwise = "." ++ createRow (x-1) y p
 
 visualisation :: Int -> Int -> [Point] -> [String]
-visualisation x 0 p
+visualisation x (-1) p
     = []
 visualisation x y p
-    = createRow x y p : visualisation x (y-1) p
+    = reverse(createRow x y p) : visualisation x (y-1) p
 
 main :: IO ()
 main
@@ -34,4 +39,6 @@ main
     -- = putStrLn (pretty  [ [ [ 'a','b' ], [ 'c','d' ] ]
                         -- , [ [ 'e','f' ], [ 'g','h' ] ]
                         -- , [ [ 'i','j' ], [ 'k','l' ] ] ])
-       = putStrLn(show(visualisation 10 5 glider))
+    = putStrLn(pretty(reverse(visualisation 5 5 glider)))
+    -- = putStrLn(createRow 5 5 glider)
+    -- = putStrLn(show(isCell 0 0 glider))
