414908f59d2a64ff5acbff1d050b99776bfa3803
Will09122000 <will0912@icloud.com>
2020-10-13 12:08:18 +0100

    Q2

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,10 +1,37 @@
-pretty :: [[[Char]]] -> IO()
-pretty (x:y:z:xs)
-    = putStr(unlines x ++ unlines y ++ unlines z) 
+type Point
+    = ( Int, Int )
+
+pretty :: [[a]] -> String
+pretty (x:xs)
+    -- = putStr(unlines x ++ unlines y ++ unlines z) 
+    = x >> pretty xs
+
+glider :: [ Point ]
+glider
+    = [ (0, 2), (1, 3), (2, 1), (2, 2), (2, 3) ]
+
+isCell :: Int -> Int -> Point -> Bool
+isCell x y p
+    = if x == fst p && y == snd p
+        then True
+
+createRow :: Int -> Int -> [Point] -> String
+createRow 0 y p:ps
+    = []
+createRow x y p:ps
+    | isCell x y p == True = "#" ++ createRow (x-1) y ps
+    | otherwise = "." ++ createRow (x-1) y ps
+
+visualisation :: Int -> Int -> [Point] -> [String]
+visualisation x 0 p
+    = []
+visualisation x y p
+    = createRow x y p : visualisation x (y-1) p
 
 main :: IO ()
 main
     -- = putStrLn (pretty (take 8 (visualisation 5 5 (evolution glider))))
-    = pretty  [ [ [ 'a','b' ], [ 'c','d' ] ]
-              , [ [ 'e','f' ], [ 'g','h' ] ]
-              , [ [ 'i','j' ], [ 'k','l' ] ] ]
+    -- = putStrLn (pretty  [ [ [ 'a','b' ], [ 'c','d' ] ]
+                        -- , [ [ 'e','f' ], [ 'g','h' ] ]
+                        -- , [ [ 'i','j' ], [ 'k','l' ] ] ])
+       = putStrLn(show(visualisation 10 5 glider))
