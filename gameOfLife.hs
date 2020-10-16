38a65a7407a178302acdea16d6fd8061496fa72d
Will09122000 <will0912@icloud.com>
2020-10-16 13:58:05 +0100

    Q2 Complete

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,4 +1,4 @@
-import Data.List.nub
+import Data.List
 
 type Point
     = ( Int, Int )
@@ -8,16 +8,16 @@ pretty []
     = []
 pretty (x:xs)
     = unlines x ++ pretty xs
-    -- Thought line below may be more suitable to create a gap between each evolution but doesn't follow spec.
+    -- Thought line below may be more suitable to create a gap between each evolution but wasn't sure if it followed the spec exactly.
     -- = unlines x ++ "\n" ++ pretty xs
 
 glider :: [Point]
 glider
-    = [ (0, 2), (1, 3), (2, 1), (2, 2), (2, 3) ]
+    = [  (0, 2), (1, 3), (2, 1), (2, 2), (2, 3) ]
 
 
 isCell :: Int -> Int -> [Point] -> Bool
-isCell x y []
+isCell _ _ []
     = False
 isCell x y (p:ps) = 
     if x == fst p && y == snd p
@@ -26,28 +26,28 @@ isCell x y (p:ps) =
         isCell x y ps
 
 createRow :: Int -> Int -> [Point] -> String
-createRow (-1) y p
+createRow (-1) _ _
     = []
 createRow x y p
     | isCell x y p = createRow (x-1) y p ++ "#"
     | otherwise = createRow (x-1) y p ++ "."
 
 createGrid :: Int -> Int -> [Point] -> [String]
-createGrid x (-1) p
+createGrid _ (-1) _
     = []
 createGrid x y p
     = createGrid x (y-1) p ++ [createRow x y p]
 
 visualisation :: Int -> Int -> [[Point]] -> [[String]]
-visualisation x y []
+visualisation _ _ []
     = []
-visualisation x (-1) p
+visualisation _ (-1) _
     = []
 visualisation x y (p:ps)
     = [createGrid x y p] ++ visualisation x y ps
 
 numNeighbours ::  Point -> [Point] -> Int
-numNeighbours p []
+numNeighbours _ []
     = 0
 numNeighbours p (x:xs)
     | abs(fst x - fst p) <= 1 && abs(snd x - snd p) <= 1 = 1 + numNeighbours p xs
@@ -61,27 +61,29 @@ rule1 (x:xs) p
     | otherwise = rule1 (xs) p
 
 checkRow :: Int -> Int -> [Point] -> [Point]
-checkRow (-1) y p
+checkRow (-1) _ _
     = []
 checkRow x y p
     | numNeighbours (x,y) p == 3 = checkRow (x-1) y p ++ [(x,y)]
     | otherwise = checkRow (x-1) y p
 
 rule2 :: Int -> Int -> [Point] -> [Point]
-rule2 x (-1) p
+rule2 _ (-1) _
     = []
 rule2 x y p
     = rule2 x (y-1) p ++ checkRow x y p
 
 evolution :: [Point] -> [[Point]]
 evolution p
-    = nub [concat[[rule1 p p], [rule2 (length p) (length p) p]]] ++ evolution p
+    = [p] ++ evolution nextP
+    where nextP = nub (concat[rule1 p p, rule2 (length p) (length p) p])
 
 main :: IO ()
 main
-    -- = putStrLn (pretty (take 3 (visualisation 5 5 (evolution glider))))
-    = putStrLn(show(rule2 (length glider) (length glider) glider))
-    -- = putStrLn(show(evolution glider))
+    = putStrLn (pretty (take 8 (visualisation 5 5 (evolution glider))))
+    -- = putStrLn ( show( nub (concat[rule1 glider glider, rule2 (length glider) (length glider) glider])))
+    -- = putStrLn(show(glider))
+    -- = putStrLn(show(rule2 (length glider) (length glider) glider))
     -- = putStrLn(show(rule1 glider))
     -- = putStrLn (pretty  [ [ [ 'a','b' ], [ 'c','d' ] ]
                         -- , [ [ 'e','f' ], [ 'g','h' ] ]
